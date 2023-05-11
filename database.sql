8563375a44047983d5ecf010240549a9703f3721
Heng Yang <hengya03@gmail.com>
2023-05-11 14:11:19 -0500

    updated database.sql

--- database.sql
+++ database.sql
@@ -11,4 +11,4 @@ CREATE TABLE "category" (
 
 -- Default categories. You may change them :)
 INSERT INTO "category" ("name")
-VALUES ('funny'), ('cohort'), ('cartoon'), ('nsfw'), ('meme');
+VALUES ('funny'), ('cohort'), ('cartoon'), ('cute_dogs'), ('meme');
