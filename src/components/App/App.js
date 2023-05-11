91303d71cde9d30d87dbb83742bfb8fd8542e447
Heng Yang <hengya03@gmail.com>
2023-05-11 15:30:16 -0500

    User input captured evertime the 'search' button is pressed. Also, the searchResultsArray is properly mapped onto the DOM.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+
 
 function App(props) {
+
+  const [userInput, setUserInput] = useState('');
+  const [searchResults, setSearchResults] = useState(['1', '2', '3']);
+
+  useEffect(() => {
+
+  }, [])
+
+  const handleClick = () => {
+    console.log('User INPUT:', userInput);
+    // axios.get(‘/gifs’)
+    //   .then((response) => {  // response = {url: ‘the url of the gif’, title: ‘the title’}
+    //     console.log(‘send success:’, response);
+    //     let url = response.data.url;
+    //     setGifUrl(url);
+    //     let title = response.data.title;
+    //     setGifTitle(title);
+    //   })
+    //   .catch((err) => { console.log(err) })
+  };
+
   return (
     <div>
       <h1>Giphy Search!</h1>
+      <input
+        placeholder='search' typeof='text'
+        value={userInput}
+        onChange={event => setUserInput(event.target.value)}
+      />
+      <button onClick={handleClick}>Search </button>
+      <div>
+      {searchResults.map((gif) => (
+            <h2>INSERT GIF HERE</h2>
+          ))}
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+
+
+// Provider allows us to use redux within our react app
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+// Import saga middleware
+import createSagaMiddleware from 'redux-saga';
+import axios from 'axios';
+import { takeLatest, put } from "redux-saga/effects";
+
+//make reducers:
+const gifs = (state = [], action) => { 
+    // if (action.type === 'GET_GIFS') {
+    //     return Number(action.payload);
+    // }
+    return state;
+};
+
+// Create sagaMiddleware
+const sagaMiddleware = createSagaMiddleware();
+
+//make a store for reducers:
+const gifsStore = createStore(
+    combineReducers({
+        gifs
+    }),
+    applyMiddleware(logger, sagaMiddleware)
+);
+// Pass rootSaga into our sagaMiddleware
+sagaMiddleware.run(rootSaga);
+
+// Create the rootSaga generator function
+function* rootSaga() {
+    yield takeLatest('NAME !', fetchGifs)
+}
+
+// SAGA functions:
+function* fetchGifs() {
+    try {
+        const response = yield axios({
+            method: 'GET',
+            url: '/fruit'
+        })
+        console.log('>>>>>>>', response.data);
+
+        //const theBasket = response.data;
+        yield put({
+            type: 'SET_GIFS',
+            payload: response.data
+        })
+    } catch (error) {
+        console.log('Something broke SAGA/GET', error)
+    }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <Provider store={gifsStore}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Provider>
 );
